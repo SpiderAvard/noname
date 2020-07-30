84171b70c6e7202f738cbf5a3d95f531457ac38a
SpiderAvard <67287491+SpiderAvard@users.noreply.github.com>
2020-07-30 21:29:46 +0300

    Add files via upload

--- Wood_planks.java
+++ Wood_planks.java
@@ -2,7 +2,10 @@ package com.avard.tame_them.Blocks;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import com.avard.tame_them.main;
 
@@ -10,14 +13,17 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
 
 public class Wood_planks extends Block
 {
-    @SideOnly(Side.CLIENT)
-    private IIcon[] field_150095_b;
+    //@SideOnly(Side.CLIENT)
+    public IIcon[] field_150095_b;
 
     public Wood_planks()
     {
@@ -47,28 +53,30 @@ public class Wood_planks extends Block
     {
         return p_149692_1_;
     }
+    
+    
+    public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune)
+    {
+        ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
+
+        ret.add(new ItemStack(Item.getItemFromBlock(this), 1, metadata));
+        return ret;
+    }
 
     /**
      * returns a list of blocks with the same ID, but different meta (eg: wood returns 4 blocks)
      */
-    @SideOnly(Side.CLIENT)
-    public void getSubBlocks(Item p_149666_1_, CreativeTabs p_149666_2_, List p_149666_3_)
+    //@SideOnly(Side.CLIENT)
+    public void getSubBlocks(Item p_150895_1_, CreativeTabs p_150895_2_, List p_150895_3_)
     {
-        p_149666_3_.add(new ItemStack(p_149666_1_, 1, 0));
-        p_149666_3_.add(new ItemStack(p_149666_1_, 1, 1));
-        p_149666_3_.add(new ItemStack(p_149666_1_, 1, 2));
-        p_149666_3_.add(new ItemStack(p_149666_1_, 1, 3));
-        p_149666_3_.add(new ItemStack(p_149666_1_, 1, 4));
-        p_149666_3_.add(new ItemStack(p_149666_1_, 1, 5));
-        p_149666_3_.add(new ItemStack(p_149666_1_, 1, 6));
-        p_149666_3_.add(new ItemStack(p_149666_1_, 1, 7));
-        p_149666_3_.add(new ItemStack(p_149666_1_, 1, 8));
-        p_149666_3_.add(new ItemStack(p_149666_1_, 1, 9));
-        p_149666_3_.add(new ItemStack(p_149666_1_, 1, 10));
-        p_149666_3_.add(new ItemStack(p_149666_1_, 1, 11));
-        p_149666_3_.add(new ItemStack(p_149666_1_, 1, 12));
-        p_149666_3_.add(new ItemStack(p_149666_1_, 1, 13));
-        p_149666_3_.add(new ItemStack(p_149666_1_, 1, 14));
+        for (int i = 0; i < 15; ++i)
+        {
+            p_150895_3_.add(new ItemStack(p_150895_1_, 1, i));
+        }
+    }
+    
+    public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entity, ItemStack itemStack) {
+    	world.setBlockMetadataWithNotify(x, y, z, itemStack.getItemDamage(), 3);
     }
 
     @SideOnly(Side.CLIENT)
